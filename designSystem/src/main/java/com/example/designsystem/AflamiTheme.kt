88908c64b4c14482d0e6f8286c914fb79b9312b5
Mona <monaayman584@gmail.com>
2025-07-01 17:56:22 +0300

    feature: add AflamiTheme.kt for textStyle and color

--- designSystem/src/main/java/com/example/designsystem/AflamiTheme.kt
+++ designSystem/src/main/java/com/example/designsystem/AflamiTheme.kt
@@ -1,4 +1,19 @@
 package com.example.designsystem
 
-class AflamiTheme {
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.Color
+import com.amsterdam.aflami.theme.textStyle.AflamiTextStyle
+import com.amsterdam.aflami.theme.textStyle.LocalAflamiTextStyle
+
+typealias ColorType = @Composable () -> Color
+
+object AflamiTheme{
+// val color: AflamiAppColor
+//     @Composable @ReadOnlyComposable get() = LocalAflamiLocalColors.current
+
+val textStyle: AflamiTextStyle
+    @Composable @ReadOnlyComposable get() = LocalAflamiTextStyle.current
+//  val images: AflamiAppImages
+//      @Composable @ReadOnlyComposable get() = LocalAflamiLocalImages.current
 }
