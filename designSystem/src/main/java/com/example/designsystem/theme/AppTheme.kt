4c73bc54e2b1412cadce0e9dac97c6688c31bf0a
Thoraya <thoriahamdy99@gmail.com>
2025-07-02 20:50:03 +0300

    fix: resolve problem of removing aflami theme.

---
+++ designSystem/src/main/java/com/example/designsystem/theme/AppTheme.kt
@@ -0,0 +1,18 @@
+package com.example.designsystem.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import com.example.designsystem.theme.colors.AflamiColorScheme
+import com.example.designsystem.theme.colors.localAflamiAppColors
+import com.example.designsystem.theme.textStyle.AflamiTextStyle
+import com.example.designsystem.theme.textStyle.LocalAflamiTextStyle
+
+object AppTheme {
+
+    val color: AflamiColorScheme
+        @Composable @ReadOnlyComposable get() = localAflamiAppColors.current
+
+    val textStyle: AflamiTextStyle
+        @Composable @ReadOnlyComposable get() = LocalAflamiTextStyle.current
+
+}
