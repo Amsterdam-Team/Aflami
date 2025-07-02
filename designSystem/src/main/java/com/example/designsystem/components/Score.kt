386089e38cd1f57cf5b37090c892a712f9bf0d11
Thoraya <thoriahamdy99@gmail.com>
2025-07-03 00:41:10 +0300

    feature: update score component for 0 score and apply some changes.

--- designSystem/src/main/java/com/example/designsystem/components/Score.kt
+++ designSystem/src/main/java/com/example/designsystem/components/Score.kt
@@ -1,13 +1,15 @@
 package com.example.designsystem.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.designsystem.theme.AflamiTheme
 import com.example.designsystem.theme.AppTheme
@@ -15,21 +17,27 @@ import com.example.designsystem.utils.ThemeAndLocalePreviews
 
 @Composable
 fun Score(
-    text: String,
-    textColor: Color,
-    positive: Boolean,
-    backgroundColor: Color,
+    score: Int,
     modifier: Modifier = Modifier
 ) {
-    val sign = if (positive) "+" else "-"
-    Text(
-        text = sign + text,
-        style = AppTheme.textStyle.label.large,
+    val isNegative = score < 0
+    val sign = if (score > 0) "+" else ""
+    val textColor = if (isNegative) AppTheme.color.redAccent else AppTheme.color.greenAccent
+    val backgroundColor = if (isNegative) AppTheme.color.redVariant else AppTheme.color.greenVariant
+
+    Box(
         modifier = modifier
             .background(shape = CircleShape, color = backgroundColor)
-            .padding(horizontal = 14.5.dp, vertical = 10.dp),
-        color = textColor,
-    )
+            .size(44.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "$sign$score",
+            style = AppTheme.textStyle.label.large,
+            color = textColor,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Composable
@@ -37,18 +45,9 @@ fun Score(
 private fun ScorePreview() {
     AflamiTheme {
         Column {
-            Score(
-                text = 1.toString(),
-                textColor = AppTheme.color.greenAccent,
-                backgroundColor = AppTheme.color.greenVariant,
-                positive = true,
-            )
-            Score(
-                text = 1.toString(),
-                textColor = AppTheme.color.redAccent,
-                backgroundColor = AppTheme.color.redVariant,
-                positive = false,
-            )
+            Score(score = -1)
+            Score(score = 1)
+            Score(score = 0)
         }
     }
 }
