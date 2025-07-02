db2c351b54bbb79890f0cf6c3d004b248d98df75
Mona <monaayman584@gmail.com>
2025-07-02 12:45:08 +0300

    feature: add CustomRadioButton.kt

---
+++ ui/src/main/java/com/example/ui/components/CustomRadioButton.kt
@@ -0,0 +1,58 @@
+package com.example.ui.components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.designsystem.AflamiTheme
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun CustomRadioButton(
+    modifier: Modifier = Modifier,
+    isEnable: Boolean
+) {
+    var isEnable by remember { mutableStateOf(isEnable) }
+    val color = if (isEnable) AflamiTheme.color.primary else AflamiTheme.color.disable
+    Row(
+        modifier = modifier
+            .size(18.dp)
+            .clip(CircleShape)
+            .border(
+                width = 6.dp, color = color, shape = CircleShape
+            )
+            .clickable(onClick = { isEnable = !isEnable })
+    )
+    {
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomRadioButtonPreview() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CustomRadioButton(modifier = Modifier.padding(bottom = 24.dp)
+            , isEnable = true)
+        CustomRadioButton(isEnable = false)
+    }
+}
