b49945b957f7a5a94b1e6174cef46be6f79f480c
pola george <86931053+poula22@users.noreply.github.com>
2025-07-03 15:32:24 +0300

    - chore: move file to right package components

--- designSystem/src/main/java/com/example/designsystem/components/TabLayout.kt
+++ designSystem/src/main/java/com/example/designsystem/components/TabLayout.kt
@@ -1,4 +1,4 @@
-package com.example.designsystem.composable
+package com.example.designsystem.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
