ea3e990128fc4278cfcb62c644bb989d99b9bc23
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-07-04 00:52:30 +0300

    refactor: fix some issues in composable

--- designSystem/src/main/java/com/example/designsystem/components/globalSearchHub/GlobalSearchHub.kt
+++ designSystem/src/main/java/com/example/designsystem/components/globalSearchHub/GlobalSearchHub.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -24,25 +25,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.designsystem.theme.AflamiTheme
 import com.example.designsystem.theme.AppTheme
 import com.example.designsystem.utils.ThemeAndLocalePreviews
 
 @Composable
 fun GlobalSearchHub(
-    globalSearchHubUI: GlobalSearchHubUI,
-    modifier: Modifier = Modifier
+    globalSearchHubUI: GlobalSearchHubUI, modifier: Modifier = Modifier
 ) {
     Box(
         modifier = modifier
             .clip(RoundedCornerShape(16.dp))
             .background(
-                brush = Brush.verticalGradient(globalSearchHubUI.gradient),
-                alpha = 0.8f
+                brush = Brush.verticalGradient(globalSearchHubUI.gradient), alpha = 0.8f
             )
+            .height(100.dp)
     ) {
         Column(
             modifier = Modifier
@@ -56,25 +54,24 @@ fun GlobalSearchHub(
                 modifier = Modifier.height(40.dp),
                 contentScale = ContentScale.FillHeight
             )
-            Column() {
-                Text(
-                    text = stringResource(globalSearchHubUI.labelRes),
-                    style = AppTheme.textStyle.title.small,
-                    color = AppTheme.color.onPrimary
-                )
-                Text(
-                    text = stringResource(globalSearchHubUI.descriptionRes),
-                    style = AppTheme.textStyle.label.small,
-                    color = AppTheme.color.onPrimaryBody
-                )
-            }
+            Text(
+                text = stringResource(globalSearchHubUI.labelRes),
+                style = AppTheme.textStyle.title.small,
+                color = AppTheme.color.onPrimary
+            )
+            Text(
+                text = stringResource(globalSearchHubUI.descriptionRes),
+                style = AppTheme.textStyle.label.small,
+                color = AppTheme.color.onPrimaryBody
+            )
         }
         Box(
             modifier = Modifier
-                .fillMaxHeight(0.56f)
-                .fillMaxWidth(0.35f)
+                .fillMaxHeight(0.38f)
+                .fillMaxWidth(0.20f)
                 .blur(56.dp, BlurredEdgeTreatment.Unbounded)
-                .background(color = Color.White.copy(alpha = 0.5f), CircleShape)
+                .background(color = AppTheme.color.onPrimary, CircleShape)
+                .alpha(0.12f)
                 .align(Alignment.TopEnd)
         )
     }
@@ -86,8 +83,7 @@ fun GlobalSearchHub(
 private fun GlobalSearchHubWorldPreview() {
     AflamiTheme {
         GlobalSearchHub(
-            globalSearchHubUI = GlobalSearchHubUI.WORLD,
-            modifier = Modifier.size(160.dp, 100.dp)
+            globalSearchHubUI = GlobalSearchHubUI.WORLD, modifier = Modifier.size(160.dp, 100.dp)
         )
     }
 }
@@ -97,8 +93,7 @@ private fun GlobalSearchHubWorldPreview() {
 private fun GlobalSearchHubActorPreview() {
     AflamiTheme {
         GlobalSearchHub(
-            globalSearchHubUI = GlobalSearchHubUI.ACTOR,
-            modifier = Modifier.size(160.dp, 100.dp)
+            globalSearchHubUI = GlobalSearchHubUI.ACTOR, modifier = Modifier.size(160.dp, 100.dp)
         )
     }
 }
