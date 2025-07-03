006e97fe32de723456f29d3b91fc114c7d902c67
Eslam <eslam.mohamed.551999@gmail.com>
2025-07-03 15:53:23 +0300

    feature: add SnackBar.kt component

--- designSystem/src/main/java/com/example/designsystem/theme/colors/AflamiColorScheme.kt
+++ designSystem/src/main/java/com/example/designsystem/theme/colors/AflamiColorScheme.kt
@@ -36,5 +36,7 @@ data class AflamiColorScheme(
     val overlayGradient: List<Color>,
     val streakGradient: List<Color>,
     val pointsOverlayGradient: List<Color>,
+    val successSnackBarShadow: Color,
+    val failureSnackBarShadow: Color,
 )
 
