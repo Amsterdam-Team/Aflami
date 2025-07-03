25f7fd25471b1cdbac0608f21d1b80249f236443
noor serry <noor.serry.94@gmail.com>
2025-07-03 20:37:18 +0300

    Refactor: Improve Chip click behavior

--- designSystem/src/main/java/com/example/designsystem/components/Chip.kt
+++ designSystem/src/main/java/com/example/designsystem/components/Chip.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,10 +13,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -52,7 +56,11 @@ fun Chip(
                     backgroundColor,
                     RoundedCornerShape(16.dp)
                 )
-                .border(1.dp, borderColor, RoundedCornerShape(16.dp)),
+                .border(1.dp, borderColor, RoundedCornerShape(16.dp))
+                .clip(RoundedCornerShape(16.dp))
+                .clickable( interactionSource =  remember { MutableInteractionSource() },
+                    indication = ripple(bounded = true)
+                    ,onClick = onClick),
             contentAlignment = Alignment.Center
         ) {
             Icon(
@@ -64,7 +72,6 @@ fun Chip(
             text = label,
             color = labelColor,
             style = AppTheme.textStyle.label.small,
-            modifier = Modifier.clickable(onClick = onClick)
         )
     }
 }
