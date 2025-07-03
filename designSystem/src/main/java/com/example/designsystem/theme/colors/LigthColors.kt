585a460f346fcf53b29b125107d6eb472b9d89d2
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-07-03 23:58:00 +0300

    add softBlue color to design system

--- designSystem/src/main/java/com/example/designsystem/theme/colors/LigthColors.kt
+++ designSystem/src/main/java/com/example/designsystem/theme/colors/LigthColors.kt
@@ -38,6 +38,7 @@ internal val lightThemeColors = AflamiColorScheme(
     borderLinearGradient = listOf(Color(0x14FFFFFF), Color(0x3DFFFFFF)),
     successSnackBarShadow = Color(0x1F429946),
     failureSnackBarShadow = Color(0x1FBF434C),
+    softBlue = Color(0x80EFF9FE)
 )
 
 internal val localAflamiAppColors = staticCompositionLocalOf { lightThemeColors }
