d17423ac6143a91e210c1b35e081da53d4cfcf50
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-07-04 00:09:02 +0300

    refactor: remove unused import

--- designSystem/src/main/java/com/example/designsystem/components/gameCard/BlurredContent.kt
+++ designSystem/src/main/java/com/example/designsystem/components/gameCard/BlurredContent.kt
@@ -3,16 +3,13 @@ package com.example.designsystem.components.gameCard
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.graphics.graphicsLayer
 
 @Composable
 fun BlurredContent(
