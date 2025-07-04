aeb2f0b1be558a1beebc7eb5d3d38eb76fdfe450
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-07-04 12:03:01 +0300

    fix: change fixed height

--- designSystem/src/main/java/com/example/designsystem/components/guessGame/GuessCard.kt
+++ designSystem/src/main/java/com/example/designsystem/components/guessGame/GuessCard.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -83,7 +84,7 @@ fun GuessCard(
                     .clip(RoundedCornerShape(bottomEnd = 24.dp, bottomStart = 24.dp))
                     .background(AppTheme.color.surface)
                     .fillMaxWidth()
-                    .height(50.dp)
+                    .aspectRatio((336/32).toFloat())
                     .clickable(
                         onClick = onClick
                     ),
