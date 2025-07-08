e1dfcf483819170394a0bc04b30a79cc79e5adeb
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-07-08 16:27:13 +0300

    feature: add ktor client

---
+++ remoteDatasource/src/main/java/com/example/remotedatasource/client/KtorClient.kt
@@ -0,0 +1,30 @@
+package com.example.remotedatasource.client
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.utils.io.InternalAPI
+
+
+class KtorClient(
+    private val httpClient: HttpClient = HttpClient() {
+        this.withAuthInterceptor(Interceptor())
+    }
+){
+    suspend fun get(url: String): HttpResponse{
+        return httpClient.get(url)
+    }
+
+    suspend fun post(url: String, body: Any? = null): HttpResponse{
+        return httpClient.post(url){
+            contentType(ContentType.Application.Json)
+            body?.let {
+                this.setBody(it)
+            }
+        }
+    }
+}
