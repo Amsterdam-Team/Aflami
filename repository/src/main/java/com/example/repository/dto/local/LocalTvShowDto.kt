b496ac0d638593a9b311517e49a3c39e893dd3e1
noor serry <noor.serry.94@gmail.com>
2025-07-10 22:56:15 +0300

    feature: add LocalTvShowDataSource and update LocalTvShowDto

--- repository/src/main/java/com/example/repository/dto/local/LocalTvShowDto.kt
+++ repository/src/main/java/com/example/repository/dto/local/LocalTvShowDto.kt
@@ -1,13 +1,10 @@
 package com.example.repository.dto.local
 
-import com.example.entity.Category
-
 data class LocalTvShowDto (
     val id : Long,
     val name : String ,
     val description : String,
     val poster : String,
     val productionYear : Int,
-    val categories : List<Category>,
     val rating : Float
 )
