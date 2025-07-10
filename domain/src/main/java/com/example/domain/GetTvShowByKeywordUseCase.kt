fa6245dcca7b3e2ee09500a62f60ac320e132b62
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-07-10 15:16:18 +0100

    feature: add search use cases

---
+++ domain/src/main/java/com/example/domain/GetTvShowByKeywordUseCase.kt
@@ -0,0 +1,19 @@
+package com.example.domain
+
+import com.example.domain.repository.TvShowRepository
+import com.example.entity.TvShow
+
+class GetTvShowByKeywordUseCase(
+    private val tvShowRepository: TvShowRepository
+) {
+
+    suspend operator fun invoke(
+        keyword: String,
+        rating: String,
+        categoryName: String
+    ): List<TvShow> {
+        return tvShowRepository.getTvShowByKeyword(keyword)
+            .filter { it.rating.toString() == rating }
+            .filter { it.categories.any { category -> category.name == categoryName } }
+    }
+}
--- viewModel/src/main/java/com/example/viewmodel/search/SearchViewModel.kt
+++ viewModel/src/main/java/com/example/viewmodel/search/SearchViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.viewmodel.search
 
+import com.example.domain.GetMoviesByKeywordUseCase
+import com.example.domain.GetTvShowByKeywordUseCase
 import com.example.viewmodel.BaseViewModel
 
-class SearchViewModel() : BaseViewModel<SearchUiState>(SearchUiState()) {
+class SearchViewModel(
+    private val getMoviesByKeywordUseCase: GetMoviesByKeywordUseCase,
+    private val getTvShowByKeywordUseCase: GetTvShowByKeywordUseCase
+) : BaseViewModel<SearchUiState>(SearchUiState()) {
+
+
 }
