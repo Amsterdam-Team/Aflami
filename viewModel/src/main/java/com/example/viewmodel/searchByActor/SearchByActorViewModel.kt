1c5a4cb70e44cf369f26f6439e61b9818e320f24
Mona <monaayman584@gmail.com>
2025-07-10 19:52:01 +0300

    feature: add SearchByActorViewModel.kt

---
+++ viewModel/src/main/java/com/example/viewmodel/searchByActor/SearchByActorViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.viewmodel.searchByActor
+
+import com.example.viewmodel.BaseViewModel
+
+class SearchByActorViewModel() : BaseViewModel<SearchByActorUiState, SearchByActorEffect>
+    (SearchByActorUiState()) {
+
+
+}
