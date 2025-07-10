2d89aed2f8a73165501e994e604d8886fd6326c6
Sara Ashraf <saraashraf1822004@gmail.com>
2025-07-11 00:33:31 +0300

    refactor: rename CountryDto to LocalCountryDto

--- repository/src/main/java/com/example/repository/dataSource/local/CountryDataSource.kt
+++ repository/src/main/java/com/example/repository/dataSource/local/CountryDataSource.kt
@@ -2,11 +2,11 @@ package com.example.repository.datasource.local
 
 import androidx.room.Query
 import androidx.room.Upsert
-import com.example.repository.dto.local.CountryDto
+import com.example.repository.dto.local.LocalCountryDto
 
 interface CountryDataSource {
     @Query("SELECT * FROM countries")
-    fun getAllCountries(): List<CountryDto>
+    fun getAllCountries(): List<LocalCountryDto>
     @Upsert
-    fun addAllCountries(countries: List<CountryDto>)
+    fun addAllCountries(countries: List<LocalCountryDto>)
 }
