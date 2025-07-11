c014df9255ad74746cfbc2e072ce5fbd79c8e733
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-07-11 18:19:28 +0300

    feature: add GlobalSearchViewModel and related use cases to Koin modules

--- app/src/main/java/com/amsterdam/aflami/di/useCaceModule.kt
+++ app/src/main/java/com/amsterdam/aflami/di/useCaceModule.kt
@@ -1,7 +1,17 @@
 package com.amsterdam.aflami.di
 
+import com.example.domain.useCase.GetMoviesByKeywordUseCase
+import com.example.domain.useCase.GetTvShowByKeywordUseCase
+import com.example.domain.useCase.search.ClearAllRecentSearchesUseCase
+import com.example.domain.useCase.search.ClearRecentSearchUseCase
+import com.example.domain.useCase.search.GetRecentSearchesUseCase
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val useCaseModule = module{
-
+    singleOf(::GetMoviesByKeywordUseCase)
+    singleOf(::GetTvShowByKeywordUseCase)
+    singleOf(::GetRecentSearchesUseCase)
+    singleOf(::ClearRecentSearchUseCase)
+    singleOf(::ClearAllRecentSearchesUseCase)
 }
