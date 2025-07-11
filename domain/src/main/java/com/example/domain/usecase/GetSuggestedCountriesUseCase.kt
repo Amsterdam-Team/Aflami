6d0255d30d63396efde040166c2a50c82b7b0521
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-07-11 04:04:38 +0300

    feature: handle empty country exception and handle validation on another layer not the use case direct.

--- domain/src/main/java/com/example/domain/usecase/GetSuggestedCountriesUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/GetSuggestedCountriesUseCase.kt
@@ -1,16 +1,19 @@
 package com.example.domain.usecase
 
-import com.example.domain.exceptions.CountryTooShortException
 import com.example.domain.exceptions.NoSuggestedCountriesException
 import com.example.domain.repository.CountryRepository
+import com.example.domain.validation.CountryValidator
 import com.example.entity.Country
 
-class GetSuggestedCountriesUseCase(private val countryRepository: CountryRepository) {
+class GetSuggestedCountriesUseCase(
+    private val countryRepository: CountryRepository,
+    private val countryValidator: CountryValidator
+) {
 
     suspend operator fun invoke(keyword: String): List<Country> {
-        if (keyword.length < 2) throw CountryTooShortException()
+        countryValidator.validateCountry(keyword)
         val countries = countryRepository.getSuggestedCountries()
-        return findCountryByName(countries, keyword).takeIf { it.isNotEmpty() }
+        return findCountryByName(countries, keyword.trim()).takeIf { it.isNotEmpty() }
             ?: throw NoSuggestedCountriesException()
     }
 
