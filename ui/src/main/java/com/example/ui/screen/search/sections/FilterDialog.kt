f5061e00efbdb73acbec902c7e10ea7d70afac27
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-07-11 19:31:54 +0300

    refactor: remove redundant previews from FilterDialog

--- ui/src/main/java/com/example/ui/screen/search/sections/FilterDialog.kt
+++ ui/src/main/java/com/example/ui/screen/search/sections/FilterDialog.kt
@@ -21,8 +21,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -173,8 +171,6 @@ private fun RatingBar(
 
 
 @Composable
-@Preview(widthDp = 320)
-@PreviewScreenSizes
 @ThemeAndLocalePreviews
 fun FilterDialogPreview() {
     AflamiTheme {
