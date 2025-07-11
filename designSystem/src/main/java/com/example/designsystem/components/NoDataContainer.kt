40498cda6c531236ac2aaa01d2a1468b71fa06ee
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-07-11 16:36:31 +0300

    feature: handle screen states based on effects

--- designSystem/src/main/java/com/example/designsystem/components/NoDataContainer.kt
+++ designSystem/src/main/java/com/example/designsystem/components/NoDataContainer.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.designsystem.R
 import com.example.designsystem.theme.AflamiTheme
@@ -64,8 +65,8 @@ private fun EmptyContainerPreview() {
         ) {
             NoDataContainer(
                 imageRes = painterResource(id = R.drawable.placeholder_no_result_found),
-                title = "No search result",
-                description = "please try with another keyword."
+                title = stringResource(R.string.no_search_result),
+                description = stringResource(R.string.no_search_result_description)
             )
         }
     }
--- ui/src/main/java/com/example/ui/screens/SearchByCountryScreen.kt
+++ ui/src/main/java/com/example/ui/screens/SearchByCountryScreen.kt
@@ -1,5 +1,6 @@
 package com.example.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,6 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -19,9 +23,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.designsystem.R
 import com.example.designsystem.components.MovieCard
+import com.example.designsystem.components.NoDataContainer
+import com.example.designsystem.components.Text
 import com.example.designsystem.components.TextField
 import com.example.designsystem.components.appBar.DefaultAppBar
 import com.example.designsystem.theme.AflamiTheme
+import com.example.designsystem.theme.AppTheme
 import com.example.designsystem.utils.ThemeAndLocalePreviews
 import com.example.viewmodel.search.countrySearch.SearchByCountryEffect
 import com.example.viewmodel.search.countrySearch.SearchByCountryUiState
@@ -35,43 +42,73 @@ fun SearchByCountryScreen(
     viewModel: SearchByCountryViewModel = koinViewModel()
 ) {
     val state by viewModel.state.collectAsState()
-    HandleUiEffects(viewModel)
-    SearchByCountryScreenContent(state, viewModel::onCountryNameUpdated, modifier)
-}
+    var screenContent by remember { mutableStateOf(ScreenContent.COUNTRY_TOUR) }
+    var showLoadingCountries by remember { mutableStateOf(false) }
 
-@Composable
-private fun HandleUiEffects(viewModel: SearchByCountryViewModel) {
     LaunchedEffect(
         viewModel.effect
     ) {
         viewModel.effect.collectLatest {
             when (it) {
-                SearchByCountryEffect.NoInternetConnectionEffect -> TODO()
-                SearchByCountryEffect.LoadingMoviesEffect -> TODO()
-                SearchByCountryEffect.LoadingSuggestedCountriesEffect -> TODO()
-                SearchByCountryEffect.MoviesLoadedEffect -> TODO()
-                SearchByCountryEffect.NoMoviesEffect -> TODO()
-                SearchByCountryEffect.NoSuggestedCountriesEffect -> TODO()
-                SearchByCountryEffect.SuggestedCountriesLoadedEffect -> TODO()
-                SearchByCountryEffect.InitialEffect -> TODO()
-                SearchByCountryEffect.CountryTooShortEffect -> TODO()
+                SearchByCountryEffect.NoInternetConnectionEffect -> {
+                    screenContent = ScreenContent.NO_INTERNET_CONNECTION
+                }
+
+                SearchByCountryEffect.LoadingMoviesEffect -> {
+                    screenContent = ScreenContent.LOADING_MOVIES
+                }
+
+                SearchByCountryEffect.LoadingSuggestedCountriesEffect -> {
+                    showLoadingCountries = true
+                }
+
+                SearchByCountryEffect.MoviesLoadedEffect -> {
+                    screenContent = ScreenContent.MOVIES
+                }
+
+                SearchByCountryEffect.NoMoviesEffect -> {
+                    ScreenContent.NO_MOVIES
+                }
+
+                SearchByCountryEffect.NoSuggestedCountriesEffect -> {
+                    showLoadingCountries = false
+                }
+
+                SearchByCountryEffect.SuggestedCountriesLoadedEffect -> {
+                    showLoadingCountries = false
+                }
+
+                SearchByCountryEffect.InitialEffect -> {
+                    screenContent = ScreenContent.COUNTRY_TOUR
+                }
+
+                SearchByCountryEffect.CountryTooShortEffect -> {}
             }
         }
     }
+
+    SearchByCountryScreenContent(
+        state,
+        viewModel::onCountryNameUpdated,
+        modifier,
+        screenContent,
+        showLoadingCountries
+    )
 }
 
 @Composable
 fun SearchByCountryScreenContent(
     state: SearchByCountryUiState,
     onCountryNameUpdated: (String) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    screenContent: ScreenContent,
+    showLoadingCountries: Boolean,
 ) {
     Column {
         DefaultAppBar(
             title = stringResource(R.string.world_tour_title),
             showNavigateBackButton = true,
         )
-
         Column(
             modifier = modifier
                 .fillMaxSize()
@@ -82,27 +119,99 @@ fun SearchByCountryScreenContent(
                 hintText = stringResource(R.string.country_name_hint),
                 onValueChange = { onCountryNameUpdated(it) }
             )
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
-                contentPadding = PaddingValues(vertical = 12.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-            ) {
-                items(
-                    items = state.movies,
-                    key = { movie -> movie.id }
-                ) { movie ->
-                    MovieCard(
-                        movieImage = painterResource(R.drawable.bg_children_wearing_3d),
-                        movieType = stringResource(R.string.movie),
-                        movieYear = movie.productionYear,
-                        movieTitle = movie.name,
-                        movieRating = movie.rating,
-                    )
-                }
+
+            when (screenContent) {
+                ScreenContent.COUNTRY_TOUR -> ExploreCountries()
+                ScreenContent.LOADING_MOVIES -> LoadingMovies()
+                ScreenContent.NO_INTERNET_CONNECTION -> NoInternetConnection()
+                ScreenContent.NO_MOVIES -> NoMoviesFound()
+                ScreenContent.MOVIES -> SearchedMovies(state)
             }
         }
     }
+}
+
+@Composable
+private fun CountriesList(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Composable
+private fun ExploreCountries(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.img_news),
+            contentDescription = stringResource(R.string.country_tour_image_description)
+        )
+        Text(
+            text = stringResource(R.string.country_tour_title),
+            modifier = Modifier.padding(top = 16.dp),
+            style = AppTheme.textStyle.title.medium,
+            color = AppTheme.color.title
+        )
+        Text(
+            text = stringResource(R.string.country_tour_description),
+            modifier = Modifier.padding(top = 8.dp),
+            style = AppTheme.textStyle.body.small,
+            color = AppTheme.color.body
+        )
+    }
+}
+
+@Composable
+private fun LoadingMovies() {
+
+}
+
+@Composable
+private fun NoInternetConnection() {
+
+}
+
+@Composable
+private fun NoMoviesFound() {
+    NoDataContainer(
+        title = stringResource(R.string.no_search_result),
+        description = stringResource(R.string.no_search_result_for_country),
+        imageRes = painterResource(id = R.drawable.placeholder_no_result_found),
+    )
+}
+
+@Composable
+private fun SearchedMovies(state: SearchByCountryUiState) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(vertical = 12.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(
+            items = state.movies,
+            key = { movie -> movie.id }
+        ) { movie ->
+            MovieCard(
+                movieImage = painterResource(R.drawable.bg_children_wearing_3d),
+                movieType = stringResource(R.string.movie),
+                movieYear = movie.productionYear,
+                movieTitle = movie.name,
+                movieRating = movie.rating,
+            )
+        }
+    }
+}
+
+enum class ScreenContent {
+    COUNTRY_TOUR,
+    LOADING_MOVIES,
+    NO_INTERNET_CONNECTION,
+    NO_MOVIES,
+    MOVIES
 }
 
 @Preview(showBackground = true)
@@ -112,7 +221,9 @@ private fun SearchByCriteriaPreview() {
     AflamiTheme {
         SearchByCountryScreenContent(
             SearchByCountryUiState(),
-            {}
+            {},
+            screenContent = ScreenContent.COUNTRY_TOUR,
+            showLoadingCountries = false
         )
     }
 }
