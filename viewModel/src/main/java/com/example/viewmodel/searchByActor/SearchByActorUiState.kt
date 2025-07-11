1b498a356e57d81589bad7d5f543c2749ea15832
Mona <monaayman584@gmail.com>
2025-07-11 20:45:40 +0300

    refactor: delete unneeded state

--- viewModel/src/main/java/com/example/viewmodel/searchByActor/SearchByActorUiState.kt
+++ viewModel/src/main/java/com/example/viewmodel/searchByActor/SearchByActorUiState.kt
@@ -2,10 +2,8 @@ package com.example.viewmodel.searchByActor
 
 import com.example.entity.Movie
 
-
 data class SearchByActorUiState(
     val isLoading:Boolean = false,
-    val isError:Boolean = false,
     val query:String="",
-    val result:List<Movie> = emptyList(),
+    val movies:List<Movie> = emptyList(),
 )
