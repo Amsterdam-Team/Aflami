90c4be773bd2d184ddcdb3fb153b7a446fbe93e5
Mona <monaayman584@gmail.com>
2025-07-11 20:47:42 +0300

    refactor: delete unneeded functions

--- viewModel/src/main/java/com/example/viewmodel/searchByActor/SearchByActorViewModel.kt
+++ viewModel/src/main/java/com/example/viewmodel/searchByActor/SearchByActorViewModel.kt
@@ -1,76 +1,59 @@
 package com.example.viewmodel.searchByActor
 
-import androidx.lifecycle.viewModelScope
+import com.example.entity.Movie
 import com.example.viewmodel.BaseViewModel
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.launch
 
 @OptIn(FlowPreview::class)
 class SearchByActorViewModel(
-   // private val getSearchByActorUseCase: GetSearchByActorUseCase
+    // private val getSearchByActorUseCase: GetSearchByActorUseCase
 ) : BaseViewModel<SearchByActorUiState, SearchByActorEffect>(SearchByActorUiState()) {
 
-    private val searchQuery = MutableStateFlow("")
-
     init {
-        viewModelScope.launch {
-            searchQuery
-                .debounce(300)
-                .collect { query ->
-                    if (query.isNotBlank()) {
-                        getResults(query)
+        tryToExecute(
+            action = {
+                state
+                    .debounce(300)
+                    .collect { state ->
+                        if (state.query.isNotBlank()) {
+                            getMoviesByActor(state.query)
+                        }
                     }
-                }
-        }
+            },
+            onSuccess = { result ->
+                // updateSearchByActorResult(result)
+            },
+            onError = {
+
+            }
+        )
     }
 
     fun onQueryChange(query: String) {
         updateState { it.copy(query = query) }
-        searchQuery.value = query
     }
 
-    private fun getResults(query: String) {
-        updateState { it.copy(isLoading = true, results = emptyList()) }
-
+    private fun getMoviesByActor(query: String) {
         tryToExecute(
-            function = {
-               // getSearchByActorUseCase(query)
+            action = {
+                // getSearchByActorUseCase.invoke(query)
             },
             onSuccess = { result ->
-                updateState {
-                    it.copy(
-                       // results = result,
-                        isLoading = false,
-                       // isEmpty = result.isEmpty()
-                    )
-                }
-
-                if (result.isEmpty()) {
-                    sendNewEffect(SearchByActorEffect.NoResultsFound)
-                }
+                //  updateSearchByActorResult(result)
             },
             onError = {
-                updateState { it.copy(isLoading = false) }
-                sendNewEffect(SearchByActorEffect.ShowError("Something went wrong"))
+
             }
         )
+    }
+
+    private fun updateSearchByActorResult(movies: List<Movie>) {
+        updateState { it.copy(movies =movies) }
     }
 
     fun onBackClicked() {
         sendNewEffect(SearchByActorEffect.NavigateBack)
     }
-
-    fun onClearQuery() {
-        updateState { it.copy(query = "", results = emptyList()) }
-        searchQuery.value = ""
-    }
-
-    fun onNavigateToDetails() {
-        sendNewEffect(SearchByActorEffect.NavigateToDetails)
-    }
 }
-
-
 
