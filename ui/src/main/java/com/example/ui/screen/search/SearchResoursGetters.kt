5587a4471a6d8cac4848054c296c6b691f1a133a
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-07-11 03:54:39 +0300

    feature: add unknown error state to SearchErrorUiState

--- ui/src/main/java/com/example/ui/screen/search/SearchResoursGetters.kt
+++ ui/src/main/java/com/example/ui/screen/search/SearchResoursGetters.kt
@@ -2,16 +2,17 @@ package com.example.ui.screen.search
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
-import com.example.viewmodel.search.SearchErrorUiState
 import com.example.ui.R
+import com.example.viewmodel.search.SearchErrorUiState
 
 @Composable
-fun getErrorMessageBySearchErrorUiState(errorUiState: SearchErrorUiState?): String{
+fun getErrorMessageBySearchErrorUiState(errorUiState: SearchErrorUiState?): String {
     return when (errorUiState) {
         SearchErrorUiState.BlankQuery -> stringResource(R.string.search_error_blank_query)
         SearchErrorUiState.InvalidCharacters -> stringResource(R.string.search_error_invalid_characters)
         SearchErrorUiState.QueryTooLong -> stringResource(R.string.search_error_query_too_long)
         SearchErrorUiState.QueryTooShort -> stringResource(R.string.search_error_query_too_short)
+        SearchErrorUiState.UnknownException -> stringResource(R.string.search_error_unknown)
         null -> ""
     }
 }
