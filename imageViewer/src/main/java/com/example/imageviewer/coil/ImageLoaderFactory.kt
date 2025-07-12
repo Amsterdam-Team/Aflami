e360d25920a18696021d2c82597c51558d43299b
marah222 <maraharam222@gmail.com>
2025-07-12 10:00:11 +0300

    ref: refactor image classification new approach

---
+++ imageViewer/src/main/java/com/example/imageviewer/coil/ImageLoaderFactory.kt
@@ -0,0 +1,23 @@
+package com.example.imageviewer.coil
+
+
+import android.content.Context
+import coil.ImageLoader
+import com.example.imageviewer.classification.policy.ImageClassifier
+import com.example.imageviewer.classification.policy.SFWImageClassifier
+import com.example.imageviewer.classification.policy.SafetyPolicy
+
+internal object ImageLoaderFactory {
+
+        fun create(context: Context, policy: SafetyPolicy): ImageLoader {
+            val classifier: ImageClassifier = when (policy) {
+                is SafetyPolicy.SFWPolicy -> SFWImageClassifier(context)
+            }
+
+            return ImageLoader.Builder(context)
+                .components {
+                    add(SafetyInterceptor(classifier))
+                }
+                .build()
+        }
+    }
