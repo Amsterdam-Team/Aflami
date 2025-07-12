386dde317537324a657b5f90f004eafe83f187ce
Mona <monaayman584@gmail.com>
2025-07-12 04:00:05 +0300

    feature: add cashing for RecentSearchRepositoryImpl.kt

--- repository/src/main/java/com/example/repository/repository/RecentSearchRepositoryImpl.kt
+++ repository/src/main/java/com/example/repository/repository/RecentSearchRepositoryImpl.kt
@@ -2,22 +2,31 @@ package com.example.repository.repository
 
 import com.example.domain.repository.RecentSearchRepository
 import com.example.repository.datasource.local.LocalRecentSearchDataSource
+import com.example.repository.dto.local.LocalSearchDto
+import com.example.repository.dto.local.utils.SearchType
+import kotlinx.datetime.Clock
 
-class RecentSearchRepositoryImpl(private val localRecentSearchDataSource: LocalRecentSearchDataSource) :
-    RecentSearchRepository {
+class RecentSearchRepositoryImpl(
+    private val localRecentSearchDataSource: LocalRecentSearchDataSource
+) : RecentSearchRepository {
     override suspend fun insertOrReplaceRecentSearch(searchKeyword: String) {
-        TODO("Not yet implemented")
+        val localSearchDto = LocalSearchDto(
+            searchKeyword = searchKeyword,
+            searchType = SearchType.BY_KEYWORD,
+            expireDate = Clock.System.now()
+        )
+        localRecentSearchDataSource.insertOrReplaceSearch(localSearchDto)
     }
 
     override suspend fun getAllRecentSearches(): List<String> {
-        TODO("Not yet implemented")
+        return localRecentSearchDataSource.getRecentSearches()
     }
 
     override suspend fun deleteAllRecentSearches() {
-        TODO("Not yet implemented")
+        localRecentSearchDataSource.deleteAllSearches()
     }
 
     override suspend fun deleteRecentSearch(searchKeyword: String) {
-        TODO("Not yet implemented")
+        localRecentSearchDataSource.deleteSearchByKeyword(searchKeyword)
     }
 }
