805ffa06f80d0357f5b9d69f9559ae0bc091a946
Mona <monaayman584@gmail.com>
2025-07-12 05:15:07 +0300

    refactor: add viewModelScope for debounce

--- viewModel/src/main/java/com/example/viewmodel/searchByActor/SearchByActorViewModel.kt
+++ viewModel/src/main/java/com/example/viewmodel/searchByActor/SearchByActorViewModel.kt
@@ -1,55 +1,57 @@
 package com.example.viewmodel.searchByActor
 
+import androidx.lifecycle.viewModelScope
+import com.example.domain.usecase.GetMoviesByActorUseCase
 import com.example.entity.Movie
 import com.example.viewmodel.BaseViewModel
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlinx.serialization.builtins.NothingSerializer
 
 @OptIn(FlowPreview::class)
 class SearchByActorViewModel(
-    // private val getSearchByActorUseCase: GetSearchByActorUseCase
+     private val getMoviesByActorUseCase: GetMoviesByActorUseCase
 ) : BaseViewModel<SearchByActorUiState, SearchByActorEffect>(SearchByActorUiState()) {
 
     init {
-        tryToExecute(
-            action = {
-                state
-                    .debounce(300)
-                    .collect { state ->
-                        if (state.query.isNotBlank()) {
-                            getMoviesByActor(state.query)
-                        }
+        viewModelScope.launch {
+            state
+                .map { it.query }
+                .debounce(300)
+                .distinctUntilChanged()
+                .collect { query ->
+                    if (query.isNotBlank()) {
+                        getMoviesByActor(query)
                     }
-            },
-            onSuccess = { result ->
-                // updateSearchByActorResult(result)
-            },
-            onError = {
-
-            }
-        )
+                }
+        }
     }
+
 
     fun onQueryChange(query: String) {
         updateState { it.copy(query = query) }
     }
 
     private fun getMoviesByActor(query: String) {
+        updateState { it.copy(isLoading = true) }
         tryToExecute(
             action = {
-                // getSearchByActorUseCase.invoke(query)
+                getMoviesByActorUseCase.invoke(query)
             },
             onSuccess = { result ->
-                //  updateSearchByActorResult(result)
+                updateSearchByActorResult(result)
             },
             onError = {
-
+                     sendNewEffect(SearchByActorEffect.NoInternetConnection)
             }
         )
     }
 
     private fun updateSearchByActorResult(movies: List<Movie>) {
-        updateState { it.copy(movies =movies) }
+        updateState { it.copy(movies =movies, isLoading = false) }
     }
 
     fun onBackClicked() {
