d59227c35c73307a62eaf68226977e441f541059
Abdallah Bakhet <abakhet2010@gmail.com>
2025-07-12 04:11:59 +0300

    feat add GetMoviesByActorUseCase

---
+++ domain/src/main/java/com/example/domain/usecase/GetMoviesByActorUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.domain.usecase
+
+import com.example.domain.repository.MovieRepository
+import com.example.entity.Movie
+
+class GetMoviesByActorUseCase(
+    private val movieRepository: MovieRepository,
+) {
+    suspend operator fun invoke(actorName: String): List<Movie> = movieRepository.getMoviesByActor(actorName)
+}
