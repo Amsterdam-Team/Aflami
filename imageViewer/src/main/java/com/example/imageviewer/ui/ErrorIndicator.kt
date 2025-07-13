d6f8ab550ea3a752045abc0afb538dcc52e36657
Eslam <eslam.mohamed.551999@gmail.com>
2025-07-13 14:32:50 +0300

    fix: fix loading & error states in SafeImageView.kt

---
+++ imageViewer/src/main/java/com/example/imageviewer/ui/ErrorIndicator.kt
@@ -0,0 +1,33 @@
+package com.example.imageviewer.ui
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.imageviewer.R
+
+@Composable
+fun ErrorIndicator(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Gray,
+    size: Dp = 48.dp,
+) {
+    Icon(
+        imageVector = ImageVector.vectorResource(id = R.drawable.ic_cancel),
+        contentDescription = null,
+        tint = tint,
+        modifier = modifier.size(size),
+    )
+}
+
+@Preview
+@Composable
+private fun LoadingIndicatorPreview() {
+    LoadingIndicator()
+}
