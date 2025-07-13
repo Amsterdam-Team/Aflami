008758d524adf7d02f131cdbc65548b52b1733b6
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-07-13 19:07:51 +0100

    refactor: remove query length check from GetMoviesByKeywordUseCase

--- domain/src/main/java/com/example/domain/useCase/GetMoviesByKeywordUseCase.kt
+++ domain/src/main/java/com/example/domain/useCase/GetMoviesByKeywordUseCase.kt
@@ -1,7 +1,6 @@
 package com.example.domain.useCase
 
 import com.example.domain.exceptions.NoMoviesByKeywordFoundException
-import com.example.domain.exceptions.QueryTooShortException
 import com.example.domain.repository.MovieRepository
 import com.example.entity.Movie
 
@@ -14,8 +13,6 @@ class GetMoviesByKeywordUseCase(
         rating: Float = 0f,
         categoryName: String = ""
     ): List<Movie> {
-        if (keyword.length < 3)
-            throw QueryTooShortException()
 
         return movieRepository.getMoviesByKeyword(keyword)
             .sortedByDescending { it.popularity }
