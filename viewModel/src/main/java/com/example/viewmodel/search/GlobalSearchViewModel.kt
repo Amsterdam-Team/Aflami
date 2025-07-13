6c4c9fb7e1085604bbac3d8d107bb68ad94adb67
Nabil Bouzine <139881384+nabilBouzineDev@users.noreply.github.com>
2025-07-13 19:50:47 +0100

    fix: use `update` for StateFlow and re-observe when tab changed (#105)

--- viewModel/src/main/java/com/example/viewmodel/search/GlobalSearchViewModel.kt
+++ viewModel/src/main/java/com/example/viewmodel/search/GlobalSearchViewModel.kt
@@ -25,6 +25,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 @OptIn(FlowPreview::class)
@@ -113,7 +114,7 @@ class GlobalSearchViewModel(
     }
 
     override fun onTextValuedChanged(text: String) {
-        _query.value = text
+        _query.update { oldText -> text }
         updateState { it.copy(query = text) }
     }
 
@@ -136,6 +137,7 @@ class GlobalSearchViewModel(
     override fun onMovieCardClicked() = sendNewEffect(SearchUiEffect.NavigateToMovieDetails)
 
     override fun onTabOptionClicked(tabOption: TabOption) {
+        observeSearchQueryChanges()
         updateState {
             it.copy(
                 selectedTabOption = tabOption,
@@ -143,7 +145,6 @@ class GlobalSearchViewModel(
                 tvShows = state.value.tvShows,
             )
         }
-        onSearchQueryChanged(state.value.query)
     }
 
     override fun onRecentSearchClicked(keyword: String) = onTextValuedChanged(keyword)
