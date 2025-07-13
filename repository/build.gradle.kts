bfda5afd654eacd95942dacf17e1233933bd6427
Sara Ashraf <saraashraf1822004@gmail.com>
2025-07-13 21:31:59 +0300

    feature: add JUnit Jupiter to repository dependencies

--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -22,6 +22,14 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     // Room
     implementation(libs.androidx.room.runtime)
+    testImplementation(libs.jupiter.junit.jupiter)
+    testImplementation(libs.jupiter.junit.jupiter)
+    testImplementation(libs.jupiter.junit.jupiter)
+    testImplementation(libs.jupiter.junit.jupiter)
+    testImplementation(libs.jupiter.junit.jupiter)
+    testImplementation(libs.jupiter.junit.jupiter)
+    testImplementation(libs.jupiter.junit.jupiter)
+    testImplementation(libs.jupiter.junit.jupiter)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
     // Date and Time
@@ -40,4 +48,5 @@ dependencies {
     //mockk
     testImplementation (libs.mockk)
     testImplementation (libs.kotlinx.coroutines.test)
+
 }
