28e6cd8ac89f90909e0e4493d2f36f1468ec7250
Sara Ashraf <saraashraf1822004@gmail.com>
2025-07-15 05:45:30 +0300

    feature: add paging source for movies by keyword and type

--- localDatasource/src/main/java/com/example/localdatasource/roomDataBase/daos/MovieDao.kt
+++ localDatasource/src/main/java/com/example/localdatasource/roomDataBase/daos/MovieDao.kt
@@ -1,5 +1,6 @@
 package com.example.localdatasource.roomDataBase.daos
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Transaction
@@ -46,4 +47,21 @@ interface MovieDao {
 
     @Upsert
     suspend fun insertSearchEntries(entries: List<SearchMovieCrossRefDto>)
+
+    @Query(
+        """
+    SELECT m.* FROM movies AS m
+    INNER JOIN search_movie_cross_ref AS x
+    ON m.movieId = x.movieId
+    WHERE x.searchKeyword = :keyword 
+      AND x.searchType = :searchType
+    ORDER BY m.movieId ASC
+"""
+    )
+    fun getMoviesPagingSourceByKeywordAndType(
+        keyword: String,
+        searchType: SearchType
+    ): PagingSource<Int, LocalMovieDto>
+
+
 }
