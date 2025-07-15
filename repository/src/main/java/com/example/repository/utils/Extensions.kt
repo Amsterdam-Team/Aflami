baf90096f26818e3b3361c4b70c11efce69d1edd
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-07-15 15:24:59 +0300

    refactor: add tryToExecute function to be used for safe calling.

---
+++ repository/src/main/java/com/example/repository/utils/Extensions.kt
@@ -0,0 +1,23 @@
+package com.example.repository.utils
+
+import com.example.domain.exceptions.AflamiException
+import com.example.domain.exceptions.UnknownException
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+internal suspend fun <T, R> tryToExecute(
+    function: suspend () -> T,
+    onSuccess: suspend (T) -> R,
+    onFailure: (AflamiException) -> R,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
+): R = withContext(dispatcher) {
+    try {
+        val result = function()
+        onSuccess(result)
+    } catch (exception: AflamiException) {
+        onFailure(exception)
+    } catch (exception: Exception) {
+        onFailure(UnknownException())
+    }
+}
