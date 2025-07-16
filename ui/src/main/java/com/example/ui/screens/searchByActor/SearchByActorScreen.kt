daa2248c6c6218680018e51b74810c301c2341c6
Eslam <eslam.mohamed.551999@gmail.com>
2025-07-16 12:23:33 +0300

    fix: fix states to be centered in screen in SearchByActorScreen.kt

--- ui/src/main/java/com/example/ui/screens/searchByActor/SearchByActorScreen.kt
+++ ui/src/main/java/com/example/ui/screens/searchByActor/SearchByActorScreen.kt
@@ -9,11 +9,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -22,11 +26,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.designsystem.R
+import com.example.designsystem.components.CenterOfScreenContainer
 import com.example.designsystem.components.MovieCard
 import com.example.designsystem.components.NoDataContainer
 import com.example.designsystem.components.NoNetworkContainer
@@ -53,13 +60,9 @@ fun SearchByActorScreen(
     LaunchedEffect(Unit) {
         viewModel.effect.collect { effect ->
             when (effect) {
-
                 SearchByActorEffect.NavigateBack -> {
                     navController.popBackStack()
                 }
-
-                SearchByActorEffect.NavigateBack -> {}
-
                 null -> {}
             }
         }
@@ -86,21 +89,30 @@ private fun SearchByActorContent(
         modifier = modifier
             .fillMaxSize()
             .statusBarsPadding()
+            .navigationBarsPadding()
             .padding(horizontal = 16.dp)
     ) {
-        DefaultAppBar(
-            title = stringResource(R.string.find_by_actor),
-            showNavigateBackButton = true,
-            onNavigateBackClicked = { interactionListener.onNavigateBackClicked() }
-        )
-        TextField(
-            text = state.keyword,
-            hintText = stringResource(R.string.find_by_actor),
-            onValueChange = { interactionListener.onKeywordValueChanged(it) },
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .padding(horizontal = 16.dp),
-        )
+        var headerHeight by remember { mutableStateOf(0.dp) }
+        val focusManager = LocalFocusManager.current
+        Column(modifier = Modifier.onSizeChanged { headerHeight = it.height.dp }) {
+            DefaultAppBar(
+                title = stringResource(R.string.find_by_actor),
+                showNavigateBackButton = true,
+                onNavigateBackClicked = { interactionListener.onNavigateBackClicked() }
+            )
+            TextField(
+                text = state.keyword,
+                hintText = stringResource(R.string.find_by_actor),
+                onValueChange = { interactionListener.onKeywordValueChanged(it) },
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        interactionListener.onKeywordValueChanged(state.keyword)
+                        focusManager.clearFocus()
+                    }
+                ),
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
 
         AnimatedContent(
             targetState = state,
@@ -108,57 +120,51 @@ private fun SearchByActorContent(
                 fadeIn(animationSpec = tween(300)) togetherWith
                         fadeOut(animationSpec = tween(300))
             },
-            label = "Content Animation"
         ) { targetState ->
-            when {
-                targetState.isLoading -> Loading(modifier = Modifier)
-                targetState.noInternetException -> NoNetworkContainer(
-                    onClickRetry = onRetryQuestClicked,
-                    modifier =
-                        Modifier
-                            .fillMaxSize()
-                            .align(Alignment.CenterHorizontally)
-                )
-
-
-                targetState.keyword.isBlank() -> {
-                    NoDataContainer(
-                        imageRes = painterResource(R.drawable.img_suggestion_magician),
-                        title = stringResource(R.string.find_by_actor),
-                        description = stringResource(R.string.find_by_actor_description),
-                        modifier = Modifier
-                            .padding(horizontal = 24.dp)
-                            .padding(top = 144.dp)
+            CenterOfScreenContainer(
+                unneededSpace = headerHeight,
+                modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())
+            ) {
+                when {
+                    targetState.isLoading -> Loading(modifier = Modifier)
+                    targetState.noInternetException -> NoNetworkContainer(
+                        onClickRetry = onRetryQuestClicked,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
                     )
+                    targetState.keyword.isBlank() -> {
+                        NoDataContainer(
+                            imageRes = painterResource(R.drawable.img_suggestion_magician),
+                            title = stringResource(R.string.find_by_actor),
+                            description = stringResource(R.string.find_by_actor_description),
+                            modifier = Modifier.padding(horizontal = 24.dp)
+                        )
+                    }
+                    targetState.movies.isEmpty() -> {
+                        NoDataContainer(
+                            imageRes = painterResource(R.drawable.placeholder_no_result_found),
+                            title = stringResource(R.string.no_search_result),
+                            description = stringResource(R.string.no_search_result_description),
+                            modifier = Modifier.padding(horizontal = 24.dp)
+                        )
+                    }
+                    else -> {}
                 }
-
-                targetState.movies.isEmpty() -> {
-                    NoDataContainer(
-                        imageRes = painterResource(R.drawable.placeholder_no_result_found),
-                        title = stringResource(R.string.no_search_result),
-                        description = stringResource(R.string.no_search_result_description),
-                        modifier = Modifier
-                            .padding(horizontal = 24.dp)
-                            .padding(top = 144.dp)
-                    )
-                }
-
-                else -> {
-                    LazyVerticalGrid(
-                        columns = GridCells.Adaptive(160.dp),
-                        verticalArrangement = Arrangement.spacedBy(8.dp),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                        contentPadding = PaddingValues(vertical = 12.dp, horizontal = 16.dp),
-                    ) {
-                        items(targetState.movies) { movie ->
-                            MovieCard(
-                                movieImage = movie.poster,
-                                movieType = "Movies",
-                                movieYear = movie.productionYear,
-                                movieTitle = movie.name,
-                                movieRating = movie.rating,
-                            )
-                        }
+            }
+            if (targetState.movies.isNotEmpty() && !targetState.isLoading) {
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(160.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(vertical = 12.dp),
+                ) {
+                    items(targetState.movies) { movie ->
+                        MovieCard(
+                            movieImage = movie.poster,
+                            movieType = stringResource(R.string.movie),
+                            movieYear = movie.productionYear,
+                            movieTitle = movie.name,
+                            movieRating = movie.rating,
+                        )
                     }
                 }
             }
@@ -174,14 +180,9 @@ private fun SearchByActorContentPreview() {
         SearchByActorContent(
             state = SearchByActorScreenState(),
             interactionListener = object : SearchByActorInteractionListener {
-                override fun onKeywordValueChanged(keyword: String) {
-                }
-
-                override fun onNavigateBackClicked() {
-                }
-
-                override fun onRetryQuestClicked() {
-                }
+                override fun onKeywordValueChanged(keyword: String) {}
+                override fun onNavigateBackClicked() {}
+                override fun onRetryQuestClicked() {}
             },
             onRetryQuestClicked = {}
         )
