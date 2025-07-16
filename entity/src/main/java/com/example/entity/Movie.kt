701c2d90b5943b4447ed3b9b20eb28d061a3aa4f
noor serry <noor.serry.94@gmail.com>
2025-07-16 09:55:26 +0300

    feature: extend Movie entity

--- entity/src/main/java/com/example/entity/Movie.kt
+++ entity/src/main/java/com/example/entity/Movie.kt
@@ -1,11 +1,14 @@
 package com.example.entity
 
-data class Movie (
-    val id : Long,
-    val name : String ,
-    val description : String,
-    val poster : String,
-    val productionYear : Int,
-    val rating : Float,
+data class Movie(
+    val id: Long,
+    val name: String,
+    val description: String,
+    val poster: String,
+    val productionYear: Int,
+    val rating: Float,
     val popularity: Double,
+    val originCountry: String,
+    val movieLength: Int,
+    val hasVideo : Boolean
 )
