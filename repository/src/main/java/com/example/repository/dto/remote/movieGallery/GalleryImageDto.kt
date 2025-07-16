ac027246de19415b3ac192c44a2eb8b1601a3939
noor serry <noor.serry.94@gmail.com>
2025-07-17 00:39:36 +0300

    fix: add SerialName annotations to GalleryImageDto

--- repository/src/main/java/com/example/repository/dto/remote/movieGallery/GalleryImageDto.kt
+++ repository/src/main/java/com/example/repository/dto/remote/movieGallery/GalleryImageDto.kt
@@ -6,10 +6,10 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class GalleryImageDto(
     @SerialName("aspect_ratio") val aspectRatio: Double,
-    val height: Int,
+    @SerialName("height") val height: Int,
     @SerialName("iso_639_1") val language: String? = null,
     @SerialName("file_path") val filePath: String,
     @SerialName("vote_average") val voteAverage: Double,
     @SerialName("vote_count") val voteCount: Int,
-    val width: Int
+    @SerialName("width") val width: Int
 )
