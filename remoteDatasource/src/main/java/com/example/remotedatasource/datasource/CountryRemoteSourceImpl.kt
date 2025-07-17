c2d008d314c6a34c1e832ff39912ef2584a10f1d
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-07-17 14:41:30 +0300

    refactor: use safeExecute from BaseRemoteSource

--- remoteDatasource/src/main/java/com/example/remotedatasource/datasource/CountryRemoteSourceImpl.kt
+++ remoteDatasource/src/main/java/com/example/remotedatasource/datasource/CountryRemoteSourceImpl.kt
@@ -1,14 +1,15 @@
 package com.example.remotedatasource.datasource
 
+import com.example.remotedatasource.base.BaseRemoteSource
 import com.example.remotedatasource.client.KtorClient
 import com.example.repository.datasource.remote.CountryRemoteSource
 import com.example.repository.dto.remote.RemoteCountryDto
 
 class CountryRemoteSourceImpl(
-    private val ktorClient: KtorClient,
-) : CountryRemoteSource {
+    ktorClient: KtorClient,
+) : BaseRemoteSource(ktorClient), CountryRemoteSource {
     override suspend fun getCountries(): List<RemoteCountryDto> {
-        return ktorClient.tryToExecute { ktorClient.get(GET_COUNTRIES) }
+        return safeExecute { ktorClient.get(GET_COUNTRIES) }
     }
 
     private companion object {
