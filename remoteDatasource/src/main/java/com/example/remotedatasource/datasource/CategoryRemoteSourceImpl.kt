39eff49f9232738b3c186b288307a807e91d0be7
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-07-17 14:41:23 +0300

    refactor: use safeExecute from BaseRemoteSource

--- remoteDatasource/src/main/java/com/example/remotedatasource/datasource/CategoryRemoteSourceImpl.kt
+++ remoteDatasource/src/main/java/com/example/remotedatasource/datasource/CategoryRemoteSourceImpl.kt
@@ -1,19 +1,21 @@
 package com.example.remotedatasource.datasource
 
+import com.example.remotedatasource.base.BaseRemoteSource
 import com.example.remotedatasource.client.KtorClient
 import com.example.repository.datasource.remote.CategoryRemoteSource
 import com.example.repository.dto.remote.RemoteCategoryResponse
 
 class CategoryRemoteSourceImpl(
-    private val ktorClient: KtorClient
-) : CategoryRemoteSource {
+    ktorClient: KtorClient
+) : BaseRemoteSource(ktorClient), CategoryRemoteSource {
     override suspend fun getMovieCategories(): RemoteCategoryResponse {
-        return ktorClient.tryToExecute { ktorClient.get(GET_MOVIE_GENRE_LIST) }
+        return safeExecute { ktorClient.get(GET_MOVIE_GENRE_LIST) }
     }
 
     override suspend fun getTvShowCategories(): RemoteCategoryResponse {
-        return ktorClient.tryToExecute { ktorClient.get(GET_TV_SHOW_GENRE_LIST) }
+        return safeExecute { ktorClient.get(GET_TV_SHOW_GENRE_LIST) }
     }
+
 
     private companion object {
         const val GET_MOVIE_GENRE_LIST = "genre/movie/list"
