b257508acccb673bb0a1da403dd904cc1c3b31db
Mona <monaayman584@gmail.com>
2025-07-17 16:54:37 +0300

    chore: remove unnecessary AndroidX and test dependencies

--- localDatasource/build.gradle.kts
+++ localDatasource/build.gradle.kts
@@ -7,25 +7,28 @@ plugins {
 
 android {
     namespace = "com.example.localdatasource"
-
 }
+
 ksp {
     arg("room.schemaLocation", "$projectDir/schemas")
 }
+
 dependencies {
     implementation(project(":repository"))
+
     // Room
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
     testImplementation(libs.androidx.room.testing)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Coroutines
     implementation(libs.kotlinx.coroutines.core)
-    implementation (libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.android)
+
     // Date and Time
     implementation(libs.kotlinx.datetime)
+
+    // Unit Testing
+    testImplementation(libs.junit)
 }
