6cca7de439b81cdc18a22462cbf5dd3e6c830884
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-07-17 17:50:46 +0300

    refactor: update recent search use cases.

--- domain/src/test/java/com/example/domain/useCase/RecentSearchesUsaCaseTest.kt
+++ domain/src/test/java/com/example/domain/useCase/RecentSearchesUsaCaseTest.kt
@@ -1,39 +1,82 @@
 package com.example.domain.useCase
 
 import com.example.domain.repository.RecentSearchRepository
+import com.example.entity.Country
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class RecentSearchesUsaCaseTest() {
+class RecentSearchesUsaCaseTest {
     private lateinit var recentSearchRepository: RecentSearchRepository
     private lateinit var recentSearchesUsaCase: RecentSearchesUsaCase
+    private lateinit var country: Country
 
     @BeforeEach
     fun setUp() {
         recentSearchRepository = mockk(relaxed = true)
+        country = Country("EGYPT", "EG")
         recentSearchesUsaCase = RecentSearchesUsaCase(recentSearchRepository)
     }
 
     @Test
-    fun `should call upsertRecentSearch when keyword is valid`() = runTest {
+    fun `should call addRecentSearch when keyword is valid`() = runTest {
         recentSearchesUsaCase.addRecentSearch("keyword")
         coVerify { recentSearchRepository.addRecentSearch(any()) }
     }
 
     @Test
-    fun `should not call upsertRecentSearch when keyword is empty`() = runTest {
+    fun `should not call addRecentSearch when keyword is empty`() = runTest {
         recentSearchesUsaCase.addRecentSearch("")
         coVerify(exactly = 0) { recentSearchRepository.addRecentSearch(any()) }
     }
 
     @Test
-    fun `should call upsertRecentSearch from recentSearchRepository when keyword is blank but not empty`() =
+    fun `should not call addRecentSearch from recentSearchRepository when keyword is blank but not empty`() =
         runTest {
             recentSearchesUsaCase.addRecentSearch("   ")
-            coVerify(exactly = 1) { recentSearchRepository.addRecentSearch("   ") }
+            coVerify(exactly = 0) { recentSearchRepository.addRecentSearch(any()) }
+        }
+
+    @Test
+    fun `should call addRecentSearchForCountry when keyword is valid`() = runTest {
+        recentSearchesUsaCase.addRecentSearchForCountry(country)
+        coVerify { recentSearchRepository.addRecentSearchForCountry(any()) }
+    }
+
+    @Test
+    fun `should not call addRecentSearchForCountry when keyword is empty`() = runTest {
+        val country = country.copy(countryIsoCode = "")
+        recentSearchesUsaCase.addRecentSearchForCountry(country)
+        coVerify(exactly = 0) { recentSearchRepository.addRecentSearchForCountry(any()) }
+    }
+
+    @Test
+    fun `should call addRecentSearchForActor when keyword is valid`() = runTest {
+        recentSearchesUsaCase.addRecentSearchForActor("keyword")
+        coVerify { recentSearchRepository.addRecentSearchForActor(any()) }
+    }
+
+    @Test
+    fun `should not call addRecentSearchForActor when keyword is empty`() = runTest {
+        recentSearchesUsaCase.addRecentSearchForActor("")
+        coVerify(exactly = 0) { recentSearchRepository.addRecentSearchForActor(any()) }
+    }
+
+    @Test
+    fun `should not call addRecentSearchForActor from recentSearchRepository when keyword is blank but not empty`() =
+        runTest {
+            recentSearchesUsaCase.addRecentSearchForActor("   ")
+            coVerify(exactly = 0) { recentSearchRepository.addRecentSearchForActor(any()) }
+        }
+
+    @Test
+    fun `should not call addRecentSearchForCountry from recentSearchRepository when keyword is blank but not empty`() =
+        runTest {
+            val country = country.copy(countryIsoCode = "  ")
+            recentSearchesUsaCase.addRecentSearchForCountry(country)
+            coVerify(exactly = 0) { recentSearchRepository.addRecentSearchForCountry(any()) }
         }
 
     @Test
