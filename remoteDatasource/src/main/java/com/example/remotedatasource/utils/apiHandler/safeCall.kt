1b9eb1e44ef244f0fd535c9b47b0476224be20aa
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-07-18 16:58:15 +0300

    refactor: add exception tree for better exception handling

--- remoteDatasource/src/main/java/com/example/remotedatasource/utils/apiHandler/safeCall.kt
+++ remoteDatasource/src/main/java/com/example/remotedatasource/utils/apiHandler/safeCall.kt
@@ -2,15 +2,50 @@ package com.example.remotedatasource.utils.apiHandler
 
 import android.util.Log
 import com.example.domain.exceptions.NoInternetException
+import com.example.domain.exceptions.ServerErrorException
+import com.example.domain.exceptions.UnknownException
+import io.ktor.client.network.sockets.SocketTimeoutException
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import kotlinx.io.IOException
+import java.net.ConnectException
 
 suspend inline fun <reified T> safeCall(execute: () -> HttpResponse): T {
-    val response = try {
-            execute()
-        } catch (e: Exception) {
-        Log.e("bk", "safeCall: $e")
+    val response: HttpResponse
+    try {
+        response = execute()
+    } catch (e: ConnectException) {
+        Log.e(
+            "safeCall",
+            "ConnectException: ${e.message}", e)
         throw NoInternetException()
-        }
-
-    return responseToResult(response)
+    } catch (e: SocketTimeoutException) {
+        Log.e(
+            "safeCall",
+            "SocketTimeoutException: ${e.message}", e)
+        throw NoInternetException()
+    } catch (e: IOException) {
+        Log.e(
+            "safeCall",
+            "IOException: ${e.message}", e)
+        throw NoInternetException()
+    } catch (e: ClientRequestException) {
+        Log.e(
+            "safeCall",
+            "ClientRequestException: ${e.response.status.value} - ${e.response.bodyAsText()}", e)
+        throw ServerErrorException()
+    } catch (e: ServerResponseException) {
+        Log.e(
+            "safeCall",
+            "ServerResponseException: ${e.response.status.value} - ${e.response.bodyAsText()}", e)
+        throw ServerErrorException()
+    } catch (e: Exception) {
+        Log.e(
+            "safeCall",
+            "Unknown Exception: ${e.message}", e)
+        throw UnknownException()
+    }
+    return parseAndHandleHttpStatus(response)
 }
