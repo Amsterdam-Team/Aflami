023839bfdf5289d55434a2843804254b999ca035
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-07-19 00:13:57 +0300

    feature: add tv show details method in TvShowsRemoteSource

--- repository/src/main/java/com/example/repository/datasource/remote/TvShowsRemoteSource.kt
+++ repository/src/main/java/com/example/repository/datasource/remote/TvShowsRemoteSource.kt
@@ -1,7 +1,28 @@
 package com.example.repository.datasource.remote
 
+import com.example.repository.dto.remote.ProductionCompanyResponse
+import com.example.repository.dto.remote.RemoteCastAndCrewResponse
+import com.example.repository.dto.remote.RemoteTvShowItemDto
 import com.example.repository.dto.remote.RemoteTvShowResponse
+import com.example.repository.dto.remote.SeasonResponse
+import com.example.repository.dto.remote.movieGallery.RemoteGalleryResponse
+import com.example.repository.dto.remote.review.ReviewsResponse
 
 interface TvShowsRemoteSource {
     suspend fun getTvShowsByKeyword(keyword: String): RemoteTvShowResponse
+
+    suspend fun getTvShowDetailsById(tvShowId: Long): RemoteTvShowItemDto
+
+    suspend fun getTvShowCast(tvShowId: Long): RemoteCastAndCrewResponse
+
+    suspend fun getSimilarTvShows(tvShowId: Long): RemoteTvShowResponse
+
+    suspend fun getTvShowReviews(tvShowId: Long): ReviewsResponse
+
+    suspend fun getTvShowGallery(tvShowId: Long): RemoteGalleryResponse
+
+    suspend fun getTvShowCompanyProduction(tvShowId: Long): ProductionCompanyResponse
+
+    suspend fun getEpisodesBySeasonNumber(tvShowId: Long, seasonNumber: Int): SeasonResponse
 }
+
