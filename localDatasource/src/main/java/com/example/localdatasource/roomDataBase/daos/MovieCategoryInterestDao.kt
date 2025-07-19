8982b7d7b8f7304852135bc1b08ce18cfa9586b5
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-07-20 00:26:41 +0300

    refactor: use @Upsert annotation for inserting data

--- localDatasource/src/main/java/com/example/localdatasource/roomDataBase/daos/MovieCategoryInterestDao.kt
+++ localDatasource/src/main/java/com/example/localdatasource/roomDataBase/daos/MovieCategoryInterestDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Upsert
 import com.example.entity.category.MovieGenre
 import com.example.repository.dto.local.LocalMovieCategoryInterestDto
 import com.example.repository.dto.local.utils.DatabaseContract
@@ -18,7 +19,7 @@ interface MovieCategoryInterestDao {
     @Query("SELECT ${DatabaseContract.INTEREST_COUNT_FIELD} FROM ${DatabaseContract.MOVIE_CATEGORY_INTEREST_TABLE} WHERE ${DatabaseContract.GENRE_FIELD} = :genre")
     suspend fun getInterestCount(genre: MovieGenre): Int?
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Upsert
     suspend fun insertInterest(entity: LocalMovieCategoryInterestDto)
 
     @Transaction
--- localDatasource/src/main/java/com/example/localdatasource/roomDataBase/daos/TvShowCategoryInterestDao.kt
+++ localDatasource/src/main/java/com/example/localdatasource/roomDataBase/daos/TvShowCategoryInterestDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Upsert
 import com.example.entity.category.TvShowGenre
 import com.example.repository.dto.local.LocalTvShowCategoryInterestDto
 import com.example.repository.dto.local.utils.DatabaseContract
@@ -18,7 +19,7 @@ interface TvShowCategoryInterestDao {
     @Query("SELECT ${DatabaseContract.INTEREST_COUNT_FIELD} FROM ${DatabaseContract.TV_SHOW_CATEGORY_INTEREST_TABLE} WHERE ${DatabaseContract.GENRE_FIELD} = :genre")
     suspend fun getInterestCount(genre: TvShowGenre): Int?
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Upsert
     suspend fun insertInterest(entity: LocalTvShowCategoryInterestDto)
 
     @Transaction
