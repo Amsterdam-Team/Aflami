46a0b6dc4ba127e50f6df5088f247f0522fb8b91
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-07-21 20:22:58 +0300

    Chore: optimize dependencies and unused imports and remove ktor as it is unused

--- remoteDatasource/src/main/java/com/example/remotedatasource/utils/apiHandler/responseCall.kt
+++ remoteDatasource/src/main/java/com/example/remotedatasource/utils/apiHandler/responseCall.kt
@@ -3,8 +3,6 @@ package com.example.remotedatasource.utils.apiHandler
 import com.example.domain.exceptions.NetworkException
 import com.example.domain.exceptions.NoInternetException
 import com.example.domain.exceptions.ServerErrorException
-import io.ktor.client.network.sockets.SocketTimeoutException
-import kotlinx.io.IOException
 import kotlinx.serialization.SerializationException
 import retrofit2.HttpException
 import java.net.ConnectException
@@ -14,11 +12,7 @@ suspend inline fun <reified T> responseCall(crossinline execute: suspend () -> T
         execute()
     } catch (e: HttpException) {
         throw ServerErrorException()
-    } catch (e: SocketTimeoutException) {
-        throw NoInternetException()
     } catch (e: ConnectException) {
-        throw NoInternetException()
-    } catch (e: IOException) {
         throw NoInternetException()
     } catch (e: SerializationException) {
         throw ServerErrorException()
