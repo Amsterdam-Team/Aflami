ac6a3830b02bae183c804e0f784e7c00a7a61ced
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-07-21 10:24:02 +0300

    Feature: implement the new  api services interfaces in remote data source implementations

--- remoteDatasource/src/main/java/com/example/remotedatasource/datasource/CountryRemoteDataSourceImpl.kt
+++ remoteDatasource/src/main/java/com/example/remotedatasource/datasource/CountryRemoteDataSourceImpl.kt
@@ -1,18 +1,14 @@
 package com.example.remotedatasource.datasource
 
-import com.example.remotedatasource.client.NetworkClient
+import com.example.remotedatasource.api.CountryApiService
 import com.example.remotedatasource.utils.apiHandler.responseCall
 import com.example.repository.datasource.remote.CountryRemoteSource
 import com.example.repository.dto.remote.RemoteCountryDto
 
 class CountryRemoteDataSourceImpl(
-    private val networkClient: NetworkClient
+    private val countryApiService: CountryApiService
 ) : CountryRemoteSource {
     override suspend fun getCountries(): List<RemoteCountryDto> {
-        return responseCall { networkClient.get(GET_COUNTRIES) }
-    }
-
-    private companion object {
-        const val GET_COUNTRIES = "configuration/countries"
+        return responseCall { countryApiService.getCountries() }
     }
 }
