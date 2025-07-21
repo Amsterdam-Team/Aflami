b7b74a890d2b6bdad544942ae4c313622e1f5c51
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-07-22 00:03:08 +0300

    feature: Move the di of service classes in a separate file.

--- app/src/main/java/com/amsterdam/aflami/di/remoteDataSourceModule.kt
+++ app/src/main/java/com/amsterdam/aflami/di/remoteDataSourceModule.kt
@@ -1,50 +1,20 @@
 package com.amsterdam.aflami.di
 
-import com.example.remotedatasource.api.CategoryApiService
-import com.example.remotedatasource.api.CountryApiService
-import com.example.remotedatasource.api.MovieApiService
-import com.example.remotedatasource.api.TvShowsApiService
-import com.example.remotedatasource.client.RetrofitClient
 import com.example.remotedatasource.datasource.CategoryRemoteDataSourceImpl
 import com.example.remotedatasource.datasource.CountryRemoteDataSourceImpl
 import com.example.remotedatasource.datasource.MovieRemoteDataSourceImpl
 import com.example.remotedatasource.datasource.TvRemoteDataSourceImpl
-
-import com.example.remotedatasource.serviceProvider.CategoryServiceProvider
-import com.example.remotedatasource.serviceProvider.CountryServiceProvider
-import com.example.remotedatasource.serviceProvider.MovieServiceProvider
-import com.example.remotedatasource.serviceProvider.TvShowsServiceProvider
-import com.example.remotedatasource.serviceProvider.implementation.CategoryServiceProviderImpl
-import com.example.remotedatasource.serviceProvider.implementation.CountryServiceProviderImpl
-import com.example.remotedatasource.serviceProvider.implementation.MovieServiceProviderImpl
-import com.example.remotedatasource.serviceProvider.implementation.TvShowsServiceProviderImpl
-
 import com.example.repository.datasource.remote.CategoryRemoteSource
 import com.example.repository.datasource.remote.CountryRemoteSource
 import com.example.repository.datasource.remote.MovieRemoteSource
 import com.example.repository.datasource.remote.TvShowsRemoteSource
-import kotlinx.serialization.json.Json
-import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val remoteDataSourceModule = module {
-    single { Json { prettyPrint = true; isLenient = true; ignoreUnknownKeys = true } }
-    singleOf(::RetrofitClient)
-
-    single { get<RetrofitClient>().movieApiService() } bind MovieApiService::class
-    single { get<RetrofitClient>().categoryApiService() } bind CategoryApiService::class
-    single { get<RetrofitClient>().countryApiService() } bind CountryApiService::class
-    single { get<RetrofitClient>().tvApiService() } bind TvShowsApiService::class
-
-    singleOf(::CategoryServiceProviderImpl) { bind<CategoryServiceProvider>() }
-    singleOf(::CountryServiceProviderImpl) { bind<CountryServiceProvider>() }
-    singleOf(::MovieServiceProviderImpl) { bind<MovieServiceProvider>() }
-    singleOf(::TvShowsServiceProviderImpl) { bind<TvShowsServiceProvider>() }
-
-    singleOf(::CategoryRemoteDataSourceImpl) { bind<CategoryRemoteSource>() }
-    singleOf(::CountryRemoteDataSourceImpl) { bind<CountryRemoteSource>() }
-    singleOf(::MovieRemoteDataSourceImpl) { bind<MovieRemoteSource>() }
-    singleOf(::TvRemoteDataSourceImpl) { bind<TvShowsRemoteSource>() }
+    singleOf(::CategoryRemoteDataSourceImpl) bind CategoryRemoteSource::class
+    singleOf(::CountryRemoteDataSourceImpl) bind CountryRemoteSource::class
+    singleOf(::MovieRemoteDataSourceImpl) bind MovieRemoteSource::class
+    singleOf(::TvRemoteDataSourceImpl) bind TvShowsRemoteSource::class
 }
