7a1fe3ec629c7342668786480da7292c8cb65285
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-07-22 20:10:06 +0300

    feature: handle mappers in repositoryModule and add missing mappers in mapperModule.

--- app/src/main/java/com/amsterdam/aflami/di/repositoryModule.kt
+++ app/src/main/java/com/amsterdam/aflami/di/repositoryModule.kt
@@ -5,21 +5,6 @@ import com.example.domain.repository.CountryRepository
 import com.example.domain.repository.MovieRepository
 import com.example.domain.repository.RecentSearchRepository
 import com.example.domain.repository.TvShowRepository
-import com.example.repository.mapper.local.CountryLocalMapper
-import com.example.repository.mapper.local.MovieCategoryLocalMapper
-import com.example.repository.mapper.local.MovieLocalMapper
-import com.example.repository.mapper.local.RecentSearchLocalMapper
-import com.example.repository.mapper.local.TvShowLocalMapper
-import com.example.repository.mapper.remote.CategoryRemoteMapper
-import com.example.repository.mapper.remote.CountryRemoteMapper
-import com.example.repository.mapper.remote.MovieRemoteMapper
-import com.example.repository.mapper.remote.TvShowRemoteMapper
-import com.example.repository.mapper.remote.CastRemoteMapper
-import com.example.repository.mapper.remote.EpisodeRemoteMapper
-import com.example.repository.mapper.remote.GalleryRemoteMapper
-import com.example.repository.mapper.remote.ProductionCompanyRemoteMapper
-import com.example.repository.mapper.remote.ReviewRemoteMapper
-import com.example.repository.mapper.remote.SeasonRemoteMapper
 import com.example.repository.repository.CategoryRepositoryImpl
 import com.example.repository.repository.CountryRepositoryImpl
 import com.example.repository.repository.MovieRepositoryImpl
@@ -32,22 +17,6 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    // Mappers
-    singleOf(::CountryLocalMapper)
-    singleOf(::CountryRemoteMapper)
-    singleOf(::MovieCategoryLocalMapper)
-    singleOf(::CategoryRemoteMapper)
-    singleOf(::MovieLocalMapper)
-    singleOf(::TvShowLocalMapper)
-    singleOf(::MovieRemoteMapper)
-    singleOf(::TvShowRemoteMapper)
-    singleOf(::RecentSearchLocalMapper)
-    singleOf(::CastRemoteMapper)
-    singleOf(::ReviewRemoteMapper)
-    singleOf(::GalleryRemoteMapper)
-    singleOf(::ProductionCompanyRemoteMapper)
-    singleOf(::EpisodeRemoteMapper)
-    singleOf(::SeasonRemoteMapper)
 
     // Handler
     singleOf(::RecentSearchHandlerImpl) bind RecentSearchHandler::class
