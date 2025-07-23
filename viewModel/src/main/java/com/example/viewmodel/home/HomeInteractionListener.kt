e14c4eafeb05e12f925458a26b6e011e6d314df4
saraashraf11 <saraashraf1822004@gmail.com>
2025-07-23 20:05:46 +0300

    feature/ add Home effect and interaction listener

--- viewModel/src/main/java/com/example/viewmodel/home/HomeInteractionListener.kt
+++ viewModel/src/main/java/com/example/viewmodel/home/HomeInteractionListener.kt
@@ -5,9 +5,10 @@ import com.example.entity.category.MovieGenre
 interface HomeInteractionListener {
     fun onClickRetryLoading()
     fun onClickSearch()
-
+    fun onClickMovie(movieId : Long)
+    fun onClickShowAllContinueWatchingMovies()
     fun onClickUpcomingMovieCard(id: Long)
     fun onChangeUpcomingMovieGenre(genre: MovieGenre)
-    fun onClickMovie(movieId : Long)
+    fun onClickContinueWatchingMovie(movieId : Long)
     fun onClickShowAllToRatedMovies()
 }
