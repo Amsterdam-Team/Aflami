3509dfe57ee58af10583b8ae359702a017197f71
Mona <monaayman584@gmail.com>
2025-08-01 02:05:28 +0300

    fix: import   SuccessMediaItemsSection and  SearchScreenHeaderSection functions

--- ui/src/main/java/com/amsterdam/ui/screens/search/keywordSearch/SearchScreen.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/search/keywordSearch/SearchScreen.kt
@@ -37,6 +37,8 @@ import com.amsterdam.ui.navigation.Route
 import com.amsterdam.ui.navigation.Route.MovieDetails
 import com.amsterdam.ui.navigation.Route.SeriesDetails
 import com.amsterdam.ui.screens.search.keywordSearch.sections.RecentSearchesSection
+import com.amsterdam.ui.screens.search.keywordSearch.sections.SearchScreenHeaderSection
+import com.amsterdam.ui.screens.search.keywordSearch.sections.SuccessMediaItemsSection
 import com.amsterdam.ui.screens.search.keywordSearch.sections.SuggestionsHubSection
 import com.amsterdam.ui.screens.search.keywordSearch.sections.filterDialog.FilterDialog
 import com.amsterdam.viewmodel.search.keywordSearch.FilterInteractionListener
