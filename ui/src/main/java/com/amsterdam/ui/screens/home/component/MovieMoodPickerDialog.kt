ff6dcdb29252cfeca407b10ef0da68f37e61e4ed
Eslam <eslam.mohamed.551999@gmail.com>
2025-08-05 00:46:14 +0300

    refactor: rename MovieMoodPickerDialog.kt

--- ui/src/main/java/com/amsterdam/ui/screens/home/component/MovieMoodPickerDialog.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/home/component/MovieMoodPickerDialog.kt
@@ -1,22 +1,17 @@
 package com.amsterdam.ui.screens.home.component
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.amsterdam.designsystem.components.Dialog
-import com.amsterdam.designsystem.components.IconButton
 import com.amsterdam.designsystem.components.ImageErrorIndicator
 import com.amsterdam.designsystem.components.ImageLoadingIndicator
 import com.amsterdam.designsystem.components.LoadingContainer
@@ -36,7 +31,7 @@ import com.amsterdam.viewmodel.shared.uiStates.MovieItemUiState
 
 @SuppressLint("ContextCastToActivity")
 @Composable
-fun MovieMoodPickerDialogDialog(
+fun MovieMoodPickerDialog(
     movie: MovieItemUiState,
     onClickViewDetails: () -> Unit,
     onClickGetAnotherMovie: () -> Unit,
@@ -133,7 +128,7 @@ private fun DialogContent(
 @ThemeAndLocalePreviews
 private fun MovieDialogPreview() {
     AflamiTheme {
-        MovieMoodPickerDialogDialog(
+        MovieMoodPickerDialog(
             movie = MovieItemUiState(),
             onDismiss = { },
             onClickViewDetails = {},
