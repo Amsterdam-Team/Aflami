d76f50403856345e62ba4e046cc1e5d8c8bc5641
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-08-12 21:47:20 +0300

    refactor: remove unused imports

--- remoteDatasource/src/main/java/com/amsterdam/remotedatasource/api/ProfileApiService.kt
+++ remoteDatasource/src/main/java/com/amsterdam/remotedatasource/api/ProfileApiService.kt
@@ -3,8 +3,6 @@ package com.amsterdam.remotedatasource.api
 import com.amsterdam.remotedatasource.utils.RequiresSessionId
 import com.amsterdam.repository.dto.remote.profile.AccountDetailsRemoteDto
 import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.Query
 
 interface ProfileApiService {
     @RequiresSessionId
