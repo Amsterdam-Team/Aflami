f60b7bfc6859cf1a76816f40476334fb9da4a7ea
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-08-12 19:37:02 +0300

    feat: add PosterGuessingGameQuestion data class and getPosterGuessingGameQuestions function to GameRepository

--- domain/src/main/java/com/amsterdam/domain/repository/GameRepository.kt
+++ domain/src/main/java/com/amsterdam/domain/repository/GameRepository.kt
@@ -5,4 +5,10 @@ import com.amsterdam.entity.Movie
 interface GameRepository {
     suspend fun getTotalUserPoints(): Int
     suspend fun getRandomMoviesWithNotNullDate(requiredMoviesNumber: Int): List<Movie>
+    suspend fun getPosterGuessingGameQuestions(questionCount: Int): List<PosterGuessingGameQuestion>
 }
+data class PosterGuessingGameQuestion(
+    val posterUrl: String,
+    val movieChoices: List<String>,
+    val correctMovieName: String
+)
