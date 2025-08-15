f6107f801426b4803835062b4fdfa50720d9bd85
saraashraf11 <saraashraf1822004@gmail.com>
2025-08-15 06:20:55 +0300

    refactor: moved test data and constants into a companion object to improve readability and maintainability for MovieWithCategoriesLocalMapperTest

--- repository/src/test/java/com/amsterdam/repository/mapper/local/MovieWithCategoriesLocalMapperTest.kt
+++ repository/src/test/java/com/amsterdam/repository/mapper/local/MovieWithCategoriesLocalMapperTest.kt
@@ -10,48 +10,56 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class MovieWithCategoriesLocalMapperTest {
-
-
     @Test
     @DisplayName("should return Movie when converting from MovieWithCategories")
     fun `toEntity should return Movie when given MovieWithCategories`() {
-        // Given
-        val localMovie = MovieWithCategories(
+        val localMovieWithCategories = movieWithCategories
+
+        val result = localMovieWithCategories.toEntity()
+
+        assertThat(result).isEqualTo(expectedDomainMovie)
+    }
+
+    companion object {
+        private const val MOVIE_ID = 101L
+        private const val MOVIE_NAME = "Inception"
+        private const val MOVIE_DESCRIPTION = "A mind-bending thriller"
+        private const val MOVIE_POSTER = "poster_url.jpg"
+        private const val MOVIE_RATING = 8.8f
+        private const val MOVIE_POPULARITY = 99.5
+        private const val MOVIE_LENGTH = 148
+        private const val MOVIE_ORIGIN_COUNTRY = "USA"
+        private const val MOVIE_STORED_LANGUAGE = "en"
+        private val MOVIE_RELEASE_DATE = LocalDate.parse("2020-01-01")
+
+        private val movieWithCategories = MovieWithCategories(
             movie = MovieLocalDto(
-                movieId = 101,
-                name = "Inception",
-                description = "A mind-bending thriller",
-                poster = "poster_url.jpg",
-                releaseDate = LocalDate.parse("2020-01-01"),
-                rating = 8.8f,
-                popularity = 99.5,
-                movieLength = 148,
-                originCountry = "USA",
-                storedLanguage = "en",
+                movieId = MOVIE_ID,
+                name = MOVIE_NAME,
+                description = MOVIE_DESCRIPTION,
+                poster = MOVIE_POSTER,
+                releaseDate = MOVIE_RELEASE_DATE,
+                rating = MOVIE_RATING,
+                popularity = MOVIE_POPULARITY,
+                movieLength = MOVIE_LENGTH,
+                originCountry = MOVIE_ORIGIN_COUNTRY,
+                storedLanguage = MOVIE_STORED_LANGUAGE,
             ),
             categories = emptyList()
         )
 
-
-        val expectedDomainMovie = Movie(
-            id = 101,
-            name = "Inception",
-            description = "A mind-bending thriller",
-            posterUrl = "poster_url.jpg",
-            rating = 8.8f,
-            popularity = 99.5,
-            runTimeInMinutes = 148,
-            originCountry = "USA",
-            releaseDate = LocalDate.parse("2020-01-01"),
+        private val expectedDomainMovie = Movie(
+            id = MOVIE_ID,
+            name = MOVIE_NAME,
+            description = MOVIE_DESCRIPTION,
+            posterUrl = MOVIE_POSTER,
+            rating = MOVIE_RATING,
+            popularity = MOVIE_POPULARITY,
+            runTimeInMinutes = MOVIE_LENGTH,
+            originCountry = MOVIE_ORIGIN_COUNTRY,
+            releaseDate = MOVIE_RELEASE_DATE,
             categories = emptyList(),
             videoUrl = "",
         )
-        //When
-        val result = localMovie.toEntity()
-        // Then
-        assertThat(result).isEqualTo(expectedDomainMovie)
-
-
     }
-
 }
