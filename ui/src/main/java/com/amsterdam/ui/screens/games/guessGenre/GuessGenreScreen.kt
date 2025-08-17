2c0afa0fe4e5efaea697b4080fcbd93242fc44d0
Noor Serry <nour.serry.94@gmail.com>
2025-08-17 23:34:52 +0300

    refactor: remove redundant Box in game screens
    This commit removes a redundant `Box` composable that was wrapping the `ConfirmButton` in `GuessByPosterScreen.kt` and `GuessGenreScreen.kt`.
    
    Additionally:
    - In `GuessGenreScreen.kt`, adjusted padding for the `ConfirmButton`.
    - In `GuessTitle.kt`, centered the title text.

--- ui/src/main/java/com/amsterdam/ui/screens/games/guessGenre/GuessGenreScreen.kt
+++ ui/src/main/java/com/amsterdam/ui/screens/games/guessGenre/GuessGenreScreen.kt
@@ -88,23 +88,15 @@ private fun GameScreenContent(
             .fillMaxSize()
             .navigationBarsPadding(),
         bottomBar = {
-            Box(
+            ConfirmButton(
+                title = stringResource(R.string.next),
+                onClick = interactionListener::onMoveToNextQuestion,
+                isEnabled = state.isNextEnabled,
+                isLoading = false,
+                isNegative = false,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                ConfirmButton(
-                    title = stringResource(R.string.next),
-                    onClick = interactionListener::onMoveToNextQuestion,
-                    isEnabled = state.isNextEnabled,
-                    isLoading = false,
-                    isNegative = false,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                )
-            }
+            )
         }
     ) { innerPadding ->
         Box {
@@ -131,7 +123,8 @@ private fun GameScreenContent(
             ) {
                 LazyColumn(
                     modifier = Modifier
-                        .fillMaxSize().statusBarsPadding()
+                        .fillMaxSize()
+                        .statusBarsPadding()
                         .padding(bottom = innerPadding.calculateBottomPadding())
                 ) {
                     item {
